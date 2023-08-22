4fdfdaa745466c07c84916f556321700efbef5ad
kanggeon123 <kanggeon216@gmail.com>
2023-08-22 12:02:06 +0900

    new Commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,51 @@
 // TODO: Include packages needed for this application
-
+const Inquirer = require('./node_modules/inquirer/lib/inquirer.js');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the Project Title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What was the motivation? Why did you build this project? What problem did you solve? What did you learn?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install the project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions for use.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the Correct License.',
+        choices: ["Apache License 2.0","GNU General Public License v3.0","MIT License","BSD 2-Clause \"Simplified\" License","BSD 3-Clause \"New\" or \"Revise\" License","Creative Commons Zero v1.0 Universal","Eclipse Public License 2.0","GNU Affero General Public License 3.0","GNU General Public License 2.0","GNU Lesser General Public License 3.0","Mozilla Public License 2.0","No License"]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  Inquirer.prompt(questions).then((answers) => {
+    writeToFile("../README.md", generateMarkdown.generateMarkdown(answers) );}).catch((err) => {
+    if (err) throw err;
+  });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,124 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "Apache License 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GNU General Public License v3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "MIT License":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "BSD 2-Clause \"Simplified\" License":
+      return "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
+      break;
+    case "BSD 3-Clause \"New\" or \"Revise\" License":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "Creative Commons Zero v1.0 Universal":
+      return "[![License: CC BY 4.0](https://licensebuttons.net/l/by/4.0/80x15.png)](https://creativecommons.org/licenses/by/4.0/)";
+      break;
+    case "Eclipse Public License 2.0":
+      return "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+      break;
+    case "GNU Affero General Public License 3.0":
+      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)";
+      break;
+    case "GNU General Public License 2.0":
+      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+      break;
+    case "GNU Lesser General Public License 3.0":
+      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)";
+      break;
+    case "Mozilla Public License 2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case "Apache License 2.0":
+      return "https://choosealicense.com/licenses/apache-2.0/";
+      break;
+    case "GNU General Public License v3.0":
+      return "https://choosealicense.com/licenses/gpl-3.0/";
+      break;
+    case "MIT License":
+      return "https://choosealicense.com/licenses/mit/";
+      break;
+    case "BSD 2-Clause \"Simplified\" License":
+      return "https://choosealicense.com/licenses/bsd-2-clause/";
+      break;
+    case "BSD 3-Clause \"New\" or \"Revise\" License":
+      return "https://choosealicense.com/licenses/bsd-3-clause/";
+      break;
+    case "Creative Commons Zero v1.0 Universal":
+      return "https://choosealicense.com/licenses/cc0-1.0/";
+      break;
+    case "Eclipse Public License 2.0":
+      return "https://choosealicense.com/licenses/epl-2.0/";
+      break;
+    case "GNU Affero General Public License 3.0":
+      return "https://choosealicense.com/licenses/agpl-3.0/";
+      break;
+    case "GNU General Public License 2.0":
+      return "https://choosealicense.com/licenses/gpl-2.0/";
+      break;
+    case "GNU Lesser General Public License 3.0":
+      return "https://choosealicense.com/licenses/lgpl-3.0/";
+      break;
+    case "Mozilla Public License 2.0":
+      return "https://choosealicense.com/licenses/mpl-2.0/";
+      break;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
+    return 'License \n Copyright (c) All rights reserved. \n Licensed under the  license.';
+  }
+  else return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Badges
+  ${renderLicenseBadge(data.license)}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Credits](#credits)
+  -[License](#license)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+
+  ## License
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseSection(data.license)}
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown};
